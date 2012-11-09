d8ab2aad1d7be8e93618bd118d92221dd309764e
imbugs <imbugs@126.com>
2012-11-09 16:01:06 +0800

    添加权限备份

--- mybin/backup.sh
+++ mybin/backup.sh
@@ -13,5 +13,5 @@ stamp=$(date +%Y%m%d)
 rm -rf $BACKDIR/db_backup_*.sql
 rm -rf $BACKDIR/blog_backup_*.tgz
 mysqldump -u$DBUSER -p$DBPASS $DBNAME > $BACKDIR/db_backup_$stamp.sql
-tar czvf $BACKDIR/blog_backup_$stamp.tgz $FILES
+tar czvfp $BACKDIR/blog_backup_$stamp.tgz $FILES
 echo "BACKUP FINISHED $stamp !"
